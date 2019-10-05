682ee32ee86360c353b9fc5639342ce5a342d332
Namo Aton <namoaton@gmail.com>
2019-10-05 09:17:42 +0300

    test

--- src/mgos_bme280.c
+++ src/mgos_bme280.c
@@ -45,7 +45,7 @@ static int8_t user_ds_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data, 
     wr_data[0] = reg_addr;
     bool ok = mgos_ds28e17_rmt_write_data_stop(dt,ds_addr,dev_id<<1,  len ,wr_data);
     LOG(LL_INFO, ("Reg_addr = %X, len to read =  %d",reg_addr,len));
-    LOG(LL_INFO, ("DS write ok = %d",ok)); uuint8_t len, uint8_t* data)
+    LOG(LL_INFO, ("DS write ok = %d",ok)); 
     ok = mgos_ds28e17_rmt_read_data_stop(dt, ds_addr, dev_id, len,  reg_data);
     LOG(LL_INFO, ("DS read ok! %d",ok));
     return ok ? 0 : -2;
