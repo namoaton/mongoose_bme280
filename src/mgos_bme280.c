992ebaa13b43f312bce545bda7912f9271322b10
Namo Aton <namoaton@gmail.com>
2019-10-19 10:20:55 +0300

    hardcoded  some values

--- src/mgos_bme280.c
+++ src/mgos_bme280.c
@@ -86,6 +86,12 @@ static int8_t user_ds_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *reg_data,
     memset(wr_data,0, sizeof(wr_data));
     wr_data[0] = reg_addr;
     memcpy(&wr_data[1],reg_data, len);
+    if (reg_addr ==0xf2 ){
+       reg_data[0] = 1;
+    }
+    if ( reg_addr == 0xf4){
+       reg_data[0]=0x27;
+    }
     bool ok = mgos_ds28e17_rmt_write_data_stop(dt,mgos_ds28e17_rmt_get_addr(),dev_id<<1,  len ,wr_data);
 //    bool ok = mgos_i2c_write_reg_n(i2c, dev_id, reg_addr, len, reg_data);
     LOG(LL_INFO, ("Reg_addr = %X, len to write =  %d",reg_addr,len));
